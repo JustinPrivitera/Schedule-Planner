60cf82b14d26276b322b391cf88760a28678a140
Justin <justinprivitera@gmail.com>
2018-10-31 12:43:14 -0700

    added strtok and begin file manipulation

---
+++ Scheduler.py
@@ -0,0 +1,22 @@
+# <date>
+# Justin Privitera
+# <name>
+# <version>
+#
+# Version History:
+# 
+#
+# <Purpose>
+#
+# <Instructions>
+
+import sys
+
+from StringToken import stringToken
+
+def scheduler(inFile, outFile)
+	inputFile = open(inFile, "r")
+	fileText = inputFile.read()
+	inputFile.close()
+	outputFile = open(outFile, "w")
+
