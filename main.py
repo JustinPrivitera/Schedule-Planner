cae8e593671c8b982945e7d40d42d2bcaca1ce26
Justin <justinprivitera@gmail.com>
2018-10-31 23:16:02 -0700

    updated readme and added notes to main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-# <date>
+# 10/31/2018
 # Justin Privitera
-# <name>
-# <version>
+# Schedule Planner
+# v1.0
 #
 # Version History:
-# <dates, versions, and descriptions>
+# 10/31/2018	v1.0	added basic functionality
 #
-# <Purpose>
+# The purpose is to replace PASS with a superior scheduling applet, with greater functionality and options.
 #
-# <Instructions>
+# run the program with the following command: "python3 main.py inFile"
 
 import sys
 
