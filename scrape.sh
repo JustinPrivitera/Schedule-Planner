ad37338904d6a5c0990f7711a558b0b699a2b5c0
Kevin Kellar <kellar.kevin@gmail.com>
2018-11-05 12:32:45 -0800

    scrape8

--- scrape.sh
+++ scrape.sh
@@ -8,7 +8,7 @@ python3 coursegen.py > happydatabase/courseurls.txt
 
 cd happydatabase
 cat courseurls.txt | sed -r 's;(http://schedules.calpoly.edu/classes_)(...-[0-9][0-9][0-9])(_next.htm);curl -o \2.htm "\1\2\3";' > coursescript.sh
-source coursescript.sh
+bash coursescript.sh
 # find . -size 0 -delete
 # ls *.htm | xargs python ../html2csv.py
 
