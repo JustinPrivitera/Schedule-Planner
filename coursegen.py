71dd17f3ed67c9b38ed9d9d45b31159b2b79501a
Kevin Kellar <kellar.kevin@gmail.com>
2018-11-05 12:31:08 -0800

    scrape6

--- coursegen.py
+++ coursegen.py
@@ -1,6 +1,5 @@
 #!/bin/env/python3
 
 for x in range(1000):
-	print("http://schedules.calpoly.edu/classes_CPE-%d_next.htm" % x)
-	print("http://schedules.calpoly.edu/classes_CSC-%d_next.htm" % x)
-	
+	print("http://schedules.calpoly.edu/classes_CPE-%.3d_next.htm" % x)
+	print("http://schedules.calpoly.edu/classes_CSC-%.3d_next.htm" % x)
