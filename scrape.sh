d4b12aed21f989a2fe9ec05452e5f2f3f9d5eb59
Kevin Kellar <kellar.kevin@gmail.com>
2018-11-05 12:23:56 -0800

    scrape3

--- scrape.sh
+++ scrape.sh
@@ -7,7 +7,8 @@ mkdir -p happydatabase
 python3 coursegen.py > happydatabase/courseurls.txt
 
 cd happydatabase
-cat courseurls.txt | sed -r 's;(http://schedules.calpoly.edu/classes_)(...-[0-9][0-9][0-9])(_next.htm);wget -O \2.htm "\1\2\3";' | bash
+cat courseurls.txt | sed -r 's;(http://schedules.calpoly.edu/classes_)(...-[0-9][0-9][0-9])(_next.htm);wget -O \2.htm "\1\2\3";' | tee coursescript.sh
+source coursescript.sh
 find . -size 0 -delete
 ls *.htm | xargs python ../html2csv.py
 
