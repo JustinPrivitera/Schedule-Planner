078210d6d566596ac5a6fd91123bf515342ae99f
Kevin Kellar <kellar.kevin@gmail.com>
2018-11-05 12:21:32 -0800

    scrape2

--- scrape.sh
+++ scrape.sh
@@ -8,7 +8,9 @@ python3 coursegen.py > happydatabase/courseurls.txt
 
 cd happydatabase
 cat courseurls.txt | sed -r 's;(http://schedules.calpoly.edu/classes_)(...-[0-9][0-9][0-9])(_next.htm);wget -O \2.htm "\1\2\3";' | bash
-find . -size  0 -print0 |xargs -0 rm --
+find . -size 0 -delete
+ls *.htm | xargs python ../html2csv.py
+
 
 
 # wget -O cpe101.htm "http://schedules.calpoly.edu/classes_CPE-101_next.htm"
